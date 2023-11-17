692731781f484be8e35cdfcffe40400775d71439
vietbh <100103409+vietbh@users.noreply.github.com>
2023-11-18 02:38:39 +0700

    Chỉnh sửa them fol views

--- angular/main.js
+++ angular/main.js
@@ -1,7 +1,9 @@
-let myApp = angular.modules('myApp',[]);
+import {route} from './router.js';
+// let myApp = angular.modules('myApp',['ngRoute']);
 
-
+// 
 
 myApp.controller('myController', function($rootScope, $scope) {
     // sử dụng $rootScope trong đây
 });
+export {main}
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-//Config 
-export default {route} ;
+let app = angular.module('myApp',[]);
+
 app.config(function ($routeProvider) {
     $routeProvider
         .when("/home", { templateUrl: "view/main.html", controller: "monHoc" })
@@ -9,4 +9,8 @@ app.config(function ($routeProvider) {
         .when("/doimatkhau", { templateUrl: "view/doimatkhau.html", controller: "doimatkhau" })
         .when("/tracnghiem/:idMH/:tenMH", { templateUrl: "view/Pagetracnghiem.html", controller: 'tnctrl' })
         .when("/:ten", { templateUrl: "view/gioithieu.html", controller: "hientin" })
+});
+// 
+app.controller(($routeProvider)=>{
+
 });
